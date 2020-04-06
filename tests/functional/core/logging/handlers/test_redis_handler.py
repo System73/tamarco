54e1b9bb58334f935a7866cb496ded5a1e915d3c
Jesús Rubio Díaz <jesus.rubio@system73.com>
2020-04-06 17:27:57 +0100

    Change format

--- tests/functional/core/logging/handlers/test_redis_handler.py
+++ tests/functional/core/logging/handlers/test_redis_handler.py
@@ -16,11 +16,14 @@ def test_redis_handler(client_redis):
             args=["args1", "args2"],
             exc_info=None,
         )
+
         time_stamp = 1_400_000_000
         log_record.created = time_stamp
         handler.enqueue(log_record)
+
     time.sleep(2)
+
     key = client_redis.keys()[0]
-    assert b'logstash' == key
+    assert b"logstash" == key
     vals = client_redis.lrange(key, 0, -1)
     assert 10 == len(vals)
